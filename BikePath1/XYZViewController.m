7bee75faed534d84dd1320298d32257f739d1476
mhuerster <mary.huerster@gmail.com>
2014-08-14 18:12:09 -0400

    add nslog user's location

--- BikePath1/XYZViewController.m
+++ BikePath1/XYZViewController.m
@@ -21,16 +21,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
                                                             longitude:151.20
                                                                  zoom:10];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
+//    NSLog(@"User's location: %@", mapView_.myLocation);
     mapView_.settings.myLocationButton = YES;
-    mapView_.settings.compassButton = YES;
+//    mapView_.settings.compassButton = YES;
     mapView_.settings.zoomGestures = YES;
     self.view = mapView_;
-    
+    NSLog(@"User's location: %@", mapView_.myLocation);
     // Creates a marker in the center of the map.
     // GMSMarker *marker = [[GMSMarker alloc] init];
     // marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
@@ -38,6 +41,11 @@
     // marker.snippet = @"Australia";
     // marker.map = mapView_;
 }
+
+//- (void)didTapMyLocationButtonForMapView: (GMSMapView*) mapView{
+//    NSLog(@"User's location: %@", mapView_.myLocation);
+////    return YES;
+//}
 
 - (void)didReceiveMemoryWarning
 {
