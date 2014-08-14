d8303ed8c00ece5133d2a1dc50ab2aff7d11a639
mhuerster <mary.huerster@gmail.com>
2014-08-14 19:28:18 -0400

    working on initializing map at user location

--- BikePath1/XYZViewController.m
+++ BikePath1/XYZViewController.m
@@ -17,29 +17,39 @@
     GMSMapView *mapView_;
 }
 
+-(void)viewWillAppear:(BOOL)animated {
+    [mapView_ addObserver:self forKeyPath:@"myLocation" options:0 context: nil];
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if([keyPath isEqualToString:@"myLocation"]) {
+        CLLocation *location = [object myLocation];
+        NSLog(@"User's location: %@", mapView_.myLocation);
+        
+        CLLocationCoordinate2D target = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude);
+    }
+}
+
+-(void)dealloc {
+    [mapView_ removeObserver:self forKeyPath:@"myLocation"];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
     
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithTarget:target
                                                                  zoom:10];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+//    mapView_ = [[GMSMapView alloc] initWithFrame:self.view.bounds];
     mapView_.myLocationEnabled = YES;
 //    NSLog(@"User's location: %@", mapView_.myLocation);
     mapView_.settings.myLocationButton = YES;
 //    mapView_.settings.compassButton = YES;
     mapView_.settings.zoomGestures = YES;
     self.view = mapView_;
-    NSLog(@"User's location: %@", mapView_.myLocation);
-    // Creates a marker in the center of the map.
-    // GMSMarker *marker = [[GMSMarker alloc] init];
-    // marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    // marker.title = @"Sydney";
-    // marker.snippet = @"Australia";
-    // marker.map = mapView_;
 }
 
 //- (void)didTapMyLocationButtonForMapView: (GMSMapView*) mapView{
