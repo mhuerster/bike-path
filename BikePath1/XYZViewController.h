cda8e2fcb8f3ca19ee2b9f8d2730b76a23925475
mhuerster <mary.huerster@gmail.com>
2014-08-14 15:42:31 -0400

    Initial Commit

---
+++ BikePath1/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  BikePath1
+//
+//  Created by Molly Huerster on 8/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+
+@end
---
+++ BikePath1/XYZViewController.m
@@ -0,0 +1,29 @@
+//
+//  XYZViewController.m
+//  BikePath1
+//
+//  Created by Molly Huerster on 8/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "XYZViewController.h"
+
+@interface XYZViewController ()
+
+@end
+
+@implementation XYZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
