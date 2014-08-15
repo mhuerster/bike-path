4d77d4c133c110612243531a54d9cdd03d9a0317
mhuerster <mary.huerster@gmail.com>
2014-08-15 10:17:53 -0400

    revert to working version - remove event listener for viewWillLoad

--- BikePath1/XYZViewController.m
+++ BikePath1/XYZViewController.m
@@ -17,45 +17,20 @@
     GMSMapView *mapView_;
 }
 
--(void)viewWillAppear:(BOOL)animated {
-    [mapView_ addObserver:self forKeyPath:@"myLocation" options:0 context: nil];
-}
-
--(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if([keyPath isEqualToString:@"myLocation"]) {
-        CLLocation *location = [object myLocation];
-        NSLog(@"User's location: %@", mapView_.myLocation);
-        
-        CLLocationCoordinate2D target = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude);
-    }
-}
-
--(void)dealloc {
-    [mapView_ removeObserver:self forKeyPath:@"myLocation"];
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithTarget:target
-                                                                 zoom:10];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.706638
+                                                            longitude:-74.009070
+                                                                 zoom:14];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-//    mapView_ = [[GMSMapView alloc] initWithFrame:self.view.bounds];
     mapView_.myLocationEnabled = YES;
-//    NSLog(@"User's location: %@", mapView_.myLocation);
     mapView_.settings.myLocationButton = YES;
-//    mapView_.settings.compassButton = YES;
     mapView_.settings.zoomGestures = YES;
     self.view = mapView_;
 }
-
-//- (void)didTapMyLocationButtonForMapView: (GMSMapView*) mapView{
-//    NSLog(@"User's location: %@", mapView_.myLocation);
-////    return YES;
-//}
 
 - (void)didReceiveMemoryWarning
 {
