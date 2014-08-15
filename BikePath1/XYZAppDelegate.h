c3d53b203388a8df8c5dd0bdccfc47b61b8f7918
mhuerster <mary.huerster@gmail.com>
2014-08-15 11:09:57 -0400

    Revert "delete duplicate files, add team names to 'created by'"
    This reverts commit c9d628bbfadf2b77008a5e4a5bcc1200ac97f110.

---
+++ BikePath1/XYZAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  XYZAppDelegate.h
+//  BikePath1
+//
+//  Created by Molly Huerster on 8/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleMaps/GoogleMaps.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
